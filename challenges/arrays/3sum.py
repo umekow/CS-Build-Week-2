12ee76c8862a6e8e4e6aee7761ca82de679b5f0e
Restyled.io <commits@restyled.io>
2020-05-05 23:08:27 +0000

    Restyled by whitespace

--- challenges/arrays/3sum.py
+++ challenges/arrays/3sum.py
@@ -2,47 +2,47 @@
 first solution using hashtables
 
 #dictionary to store differences
-	#dictionary to store sums of two values 
-	
-	#for each i in array: 
+	#dictionary to store sums of two values
+
+	#for each i in array:
 		#difference = target - element
 		#diff[difference] = element
-		
-		#for j in range(i, len(array)): 
+
+		#for j in range(i, len(array)):
 			#sum = first + element
-			
-			#if sum not in sums: 
+
+			#if sum not in sums:
 				#sums[sum] = [[i, j]]
-				
-			#else: 
+
+			#else:
 				#sums[sum].append([i, j])
-	
-	
-    
+
+
+
 this resulted in duplicate triplets and took a lot of extra time and space
 
 
 triplets = []
-	
+
 	#for each i in array:
-for i in range(0, len(arr)): 
+for i in range(0, len(arr)):
 	difference = 0 - arr[i]
 	diff[difference] = arr[i]
-		
-	for j in range(i+1, len(arr)): 
+
+	for j in range(i+1, len(arr)):
 		sum = arr[i] + arr[j]
-			
-		if sum not in sums: 
+
+		if sum not in sums:
 			sums[sum] = [[arr[i], arr[j]]]
-				
-		else: 
+
+		else:
 			sums[sum].append([arr[i], arr[j]])
 
-for j in diff: 
-  if j in sums: 
-    
+for j in diff:
+  if j in sums:
+
     triplets.extend(sums[j])
-      
+
 
 print(diff)
 print(sums)
@@ -52,25 +52,25 @@ better solution
 
 def threeNumberSum(array, targetSum):
     #create an array to store triplets
-	#sort the given array in ascending order 
-	#create a variable to sum of three elements 
-	
-	#for i in range(0, len(array) -2 ) #-2 because right will point to last  
+	#sort the given array in ascending order
+	#create a variable to sum of three elements
+
+	#for i in range(0, len(array) -2 ) #-2 because right will point to last
 		#create a variable to point to the element after current elements
 		#right = len(array) - 1
-		#while left < right:  
+		#while left < right:
 			#current_sum = array[i] + array[left] + array[right]
-			#if current_sum == targetSum: 
+			#if current_sum == targetSum:
 				#triplets.append([array[i], array[left], array[right]])
 				#left += 1
 				#right -= 1
 
-			#elif current_sum > targetSum: 
+			#elif current_sum > targetSum:
 				#right -= 1
-			#elif current_sum < targetSum: 
+			#elif current_sum < targetSum:
 				#left += 1
 
-	
+
 """
 
 
--- challenges/duplicates.py
+++ challenges/duplicates.py
@@ -5,18 +5,18 @@ class Solution:
         if there is at least one duplicate value return true
         means we do not have to iterate through the entire array
 
-        P: 
+        P:
 
         As soon as the function finds a duplicate return true
 
-        #1: 
+        #1:
             use the built in sort method on the list
-            compare two elements until we reach the end 
+            compare two elements until we reach the end
 
-        #2: 
-            use a hash table to keep track of duplicates 
+        #2:
+            use a hash table to keep track of duplicates
 
-            iterate through the list 
+            iterate through the list
 
             if key for value does not exist, create one
 
