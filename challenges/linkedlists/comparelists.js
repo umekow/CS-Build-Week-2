cf23799ec898fb2b5de57f442f772017365817fd
Restyled.io <commits@restyled.io>
2020-05-12 01:57:07 +0000

    Restyled by clang-format

--- challenges/linkedlists/comparelists.js
+++ challenges/linkedlists/comparelists.js
@@ -1,19 +1,16 @@
 function CompareLists(llist1, llist2) {
-    while(llist1 !== null && llist2 !== null){
-        if((llist1.next && !llist2.next) || (!llist1.next && llist2.next)) {
-            return 0
-        }
-
-        if (llist1.data === llist2.data) {
-            llist1 = llist1.next
-            llist2 = llist2.next
-
-
-            
-        } else {
-            return 0
-        }
+  while (llist1 !== null && llist2 !== null) {
+    if ((llist1.next && !llist2.next) || (!llist1.next && llist2.next)) {
+      return 0
     }
-    return 1
-}
 
+    if (llist1.data === llist2.data) {
+      llist1 = llist1.next
+      llist2 = llist2.next
+
+    } else {
+      return 0
+    }
+  }
+  return 1
+}
