fc73cbf2d24246c02b7efd63d33e489162476ede
Restyled.io <commits@restyled.io>
2020-05-12 01:57:08 +0000

    Restyled by prettier

--- challenges/linkedlists/comparelists.js
+++ challenges/linkedlists/comparelists.js
@@ -1,16 +1,15 @@
 function CompareLists(llist1, llist2) {
   while (llist1 !== null && llist2 !== null) {
     if ((llist1.next && !llist2.next) || (!llist1.next && llist2.next)) {
-      return 0
+      return 0;
     }
 
     if (llist1.data === llist2.data) {
-      llist1 = llist1.next
-      llist2 = llist2.next
-
+      llist1 = llist1.next;
+      llist2 = llist2.next;
     } else {
-      return 0
+      return 0;
     }
   }
-  return 1
+  return 1;
 }
